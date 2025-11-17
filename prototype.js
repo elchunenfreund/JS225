26e9cd50bc5286b537ce19ee86684fb6d30c635e
elchunen_freund <elchunenfreund@gmail.com>
2025-11-16 23:46:01 -0500

    prototype exercises

--- prototype.js
+++ prototype.js
@@ -1,19 +1,66 @@
 class Animal {}
 class Cat extends Animal {}
 
-let cat = new Cat();
-let catProto = Object.getPrototypeOf(cat);
-console.log(catProto === Cat.prototype);   // true
-
 let myProto = {
   meow() {
     console.log("Meow!")
   }
 }
 
-Object.setPrototypeOf(cat, myProto);
-catProto = Object.getPrototypeOf(cat);
+function getDefiningObject(object, propKey) {
+  let obj = object
+  while (obj !== null) {
+    if (obj.hasOwnProperty(propKey)) {
+      return obj
+    } else {
+      obj = Object.getPrototypeOf(obj)
+    }
+  }
+  return null;
+}
 
-cat.meow();
-console.log(catProto === cat.prototype)
-console.log(catProto === myProto)
+function shallowCopy(object) {
+  let copy = Object.create(Object.getPrototypeOf(object))
+  for (let prop in object) {
+    if (Object.prototype.hasOwnProperty.call(object, prop)) {
+      copy[prop] = object[prop];
+    }
+  }
+  return copy
+}
+
+function extend(destination, ...sources) {
+  sources.forEach((source) => {
+    Object.getOwnPropertyNames(source).forEach((name) => {
+      destination[name] = source[name]
+    })
+  })
+  return destination
+}
+
+let foo = {
+  a: 0,
+  b: {
+    x: 1,
+    y: 2,
+  },
+};
+
+let joe = {
+  name: 'Joe'
+};
+
+let funcs = {
+  sayHello() {
+    console.log('Hello, ' + this.name);
+  },
+
+  sayGoodBye() {
+    console.log('Goodbye, ' + this.name);
+  },
+};
+
+let object = extend({}, foo, joe, funcs);
+
+console.log(object.b.x);          // => 1
+object.sayHello();                // => Hello, Joe
