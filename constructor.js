b7e88d7ac9f5d7eb2a1c2057fc7813270d35fc9c
elchunen_freund <elchunenfreund@gmail.com>
2025-11-21 15:12:01 -0500

    constuctor

--- constructor.js
+++ constructor.js
@@ -25,10 +25,29 @@ function User(first, last) {
   this.name = `${first} ${last}`;
 }
 
-let name = 'Jane Doe';
-let user1 = new User('John', 'Doe');
-let user2 = User('John', 'Doe');
+function createObject(obj) {
+  return Object.setPrototypeOf({}, obj)
+}
 
-console.log(name);         // => Jane Doe
-console.log(user1.name);   // => John Doe
-console.log(user2.name);   // => John Doe
+Object.prototype.begetObject = function() {
+  function F() {};
+  F.prototype = this;
+  return new F()
+}
+
+function neww(constructor, args) {
+  return new constructor(...args)
+}
+
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+
+Person.prototype.greeting = function() {
+  console.log('Hello, ' + this.firstName + ' ' + this.lastName);
+};
+
+let john = neww(Person, ['John', 'Doe']);
+john.greeting();          // => Hello, John Doe
+console.log(john.constructor);         // Person(firstName, lastName) {...}
