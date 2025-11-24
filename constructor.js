fa465620aa91fbfc0df1c6e5510697f73cc1e9ad
elchunen_freund <elchunenfreund@gmail.com>
2025-11-23 23:50:16 -0500

    constructor

--- constructor.js
+++ constructor.js
@@ -39,42 +39,6 @@ function neww(constructor, args) {
   return new constructor(...args)
 }
 
-function Person(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-}
-
-Person.prototype.greeting = function() {
-  console.log('Hello, ' + this.firstName + ' ' + this.lastName);
-};
-
-function Pet(animal, name) {
-  this.animal = animal;
-  this.name = name;
-}
-
-Pet.prototype.sleep = function() {
-  console.log('I am sleeping')
-}
-
-Pet.prototype.wake = function() {
-  console.log('I am awake')
-}
-
-class Cat {
-  constructor(name = 'kitty') {
-    this.name = name
-  }
-
-  greet(name) {
-    console.log(`I am a ${this.name}`)
-  }
-
-  rename(name) {
-    this.name = name
-  }
-}
-
 class Rectangle {
   constructor(width, length) {
     this.width = width;
@@ -94,8 +58,176 @@ class Rectangle {
   };
 }
 
-let rect = new Rectangle(4, 5);
+class Square extends Rectangle {
+  constructor(n) {
+    super(n, n)
+  }
+}
 
-console.log(rect.getWidth()); // 4
-console.log(rect.getLength()); // 5
-console.log(rect.getArea()); // 20
+class Animal {
+  constructor(name, age, legs, species, status) {
+    this.name = name;
+    this.age = age;
+    this.legs = legs;
+    this.species = species;
+    this.status = status;
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old and ${this.status}.`;
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, 'cat', status)
+  }
+
+  introduce() {
+    return super.introduce() + ' Meow meow!'
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, 'dog', status)
+    this.master = master
+  }
+
+  greetMaster() {
+    return `Hello ${this.master}! Woof, woof!`
+  }
+}
+
+class Vehicle {
+  constructor(make, model, wheels) {
+    this.make = make;
+    this.model = model;
+    this.wheels = wheels;
+  }
+
+  info() {
+    return `${this.make} ${this.model}`;
+  }
+
+  getWheels() {
+    return this.wheels;
+  }
+}
+
+class Car extends Vehicle {
+  constructor(make, model) {
+    super(make, model, 4);
+  }
+}
+
+class Motorcycle extends Vehicle {
+  constructor(make, model) {
+    super(make, model, 2);
+  }
+}
+
+class Truck extends Vehicle {
+  constructor(make, model, payload) {
+    super(make, model, 6);
+    this.payload = payload;
+  }
+}
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  greeting() {
+    return `Hello, I'm ${this.name}. It's very nice to meet you.`;
+  }
+}
+
+class Shouter extends Person {
+  constructor(name) {
+    super(name);
+  }
+
+  greeting() {
+    return super.greeting().toUpperCase()
+  }
+}
+
+class Pet {
+  constructor(species, name) {
+    this.species = species;
+    this.name = name;
+  }
+
+  info() {
+    return `a ${this.species} named ${this.name}`;
+  }
+}
+
+class Owner {
+  constructor(name) {
+    this.name = name;
+    this.pets = [];
+  }
+
+  addPet(pet) {
+    this.pets.push(pet);
+  }
+
+  numberOfPets() {
+    return this.pets.length;
+  }
+}
+
+class Shelter {
+  constructor() {
+    this.adoptions = {}; // map from owner name -> Owner instance
+  }
+
+  adopt(owner, pet) {
+    owner.addPet(pet);
+    if (!this.adoptions[owner.name]) {
+      this.adoptions[owner.name] = owner;
+    }
+  }
+
+  printAdoptions() {
+    for (let name in this.adoptions) {
+      const owner = this.adoptions[name];
+      console.log(`${name} has adopted the following pets:`);
+      owner.pets.forEach(pet => console.log(pet.info()));
+      console.log('');
+    }
+  }
+}
+
+class Banner {
+  constructor(message) {
+    this.message = message;
+  }
+
+  displayBanner() {
+    console.log([
+      this.horizontalRule(),
+      this.emptyLine(),
+      this.messageLine(),
+      this.emptyLine(),
+      this.horizontalRule()
+    ].join("\n"));
+  }
+
+  horizontalRule() {
+    return `+-${'-'.repeat(this.message.length)}-+`;
+  }
+
+  emptyLine() {
+    return `| ${' '.repeat(this.message.length)} |`;
+  }
+
+  messageLine() {
+    return `| ${this.message} |`;
+  }
+}
+
+let banner1 = new Banner('To boldly go where no one has gone before.');
+banner1.displayBanner();
