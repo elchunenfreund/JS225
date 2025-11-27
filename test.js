cf84b498964ef4709af96357d253f759f950920d
Elchunen Freund <elchunenfreund@gmail.com>
2025-11-27 17:58:10 -0500

    test

--- test.js
+++ test.js
@@ -1,19 +1,113 @@
-class Classy {
-  constructor(name) {
-    this.name = name;
-  }
-}
+/* eslint-disable max-lines-per-function */
+/* eslint-disable max-len */
+let ItemManager = (function() {
+  let items = []; // Private collection
 
-class Subclassy extends Classy {
-  constructor(name, age) {
-    super(name);
-    this.age = age;
+  function findItemBySku(skuCode) {
+    return items.find((item) => item.skuCode === skuCode);
   }
 
-  describe() {
-    console.log(`Hi my name is ${this.name} and im ${this.age} years old`);
+  function findItemIndexBySku(skuCode) {
+    return items.findIndex((item) => item.skuCode === skuCode);
   }
-}
 
-let me = new Subclassy('Chunie', 25)
-me.describe()
+  return {
+    create: function(itemName, category, quantity) {
+      const validItemName = itemName.replace(/\s/g, '').length >= 5;
+      const validCategory = category.length >= 5 && !/\s/.test(category);
+      const validQuantity = typeof quantity === 'number' && quantity >= 0;
+
+      if (!validItemName || !validCategory || !validQuantity) {
+        return false;
+      }
+
+      const skuCode = (itemName.replace(/\s/g, '').slice(0, 3) + category.slice(0, 2)).toUpperCase();
+      let existingItem = findItemBySku(skuCode);
+      if (existingItem) {
+        existingItem.quantity += quantity;
+      } else {
+        items.push({
+          skuCode,
+          itemName,
+          category,
+          quantity,
+        });
+      }
+      return true;
+    },
+
+    update: function(skuCode, itemInformation) {
+      let item = findItemBySku(skuCode);
+      if (item) {
+        Object.assign(item, itemInformation);
+      }
+    },
+
+    delete: function(skuCode) {
+      let index = findItemIndexBySku(skuCode);
+      if (index !== -1) {
+        items.splice(index, 1);
+      }
+    },
+
+    items() {
+      return items.slice();
+    },
+
+    inStock() {
+      return items.filter((item) => item.quantity > 0);
+    },
+
+    itemsInCategory(category) {
+      return items.filter((item) => item.category === category);
+    },
+  };
+})();
+
+ItemManager.create('basket ball', 'sports', 0);           // valid item
+ItemManager.create('soccer ball', 'sports', 5);           // valid item
+ItemManager.create('football', 'sports', 3);              // valid item
+ItemManager.create('kitchen pot', 'cooking', 5);          // valid item
+ItemManager.create('bad item', 'sports', 0);              // invalid item name
+ItemManager.create('soccer ball', 'sports', 10);          // updates quantity of existing item
+console.log(ItemManager.items());
+// logs:
+// [
+//   { skuCode: 'BASSP', itemName: 'basket ball', category: 'sports', quantity: 0 },
+//   { skuCode: 'SOCSP', itemName: 'soccer ball', category: 'sports', quantity: 15 },
+//   { skuCode: 'FOOSP', itemName: 'football', category: 'sports', quantity: 3 },
+//   { skuCode: 'KITCO', itemName: 'kitchen pot', category: 'cooking', quantity: 5 },
+// ]
+
+ItemManager.delete('SOCSP');
+console.log(ItemManager.items());
+// logs:
+// [
+//   { skuCode: 'BASSP', itemName: 'basket ball', category: 'sports', quantity: 0 },
+//   { skuCode: 'FOOSP', itemName: 'football', category: 'sports', quantity: 3 },
+//   { skuCode: 'KITCO', itemName: 'kitchen pot', category: 'cooking', quantity: 5 },
+// ]
+
+let kitchenPot = ItemManager.items().filter(item => item.skuCode === 'KITCO')[0];
+ItemManager.update('KITCO', { quantity: 10 });
+console.log(ItemManager.items());
+// logs:
+// [
+//   { skuCode: 'BASSP', itemName: 'basket ball', category: 'sports', quantity: 0 },
+//   { skuCode: 'FOOSP', itemName: 'football', category: 'sports', quantity: 3 },
+//   { skuCode: 'KITCO', itemName: 'kitchen pot', category: 'cooking', quantity: 10 },
+// ]
+
+console.log(ItemManager.inStock());
+// logs:
+// [
+//   { skuCode: 'FOOSP', itemName: 'football', category: 'sports', quantity: 3 },
+//   { skuCode: 'KITCO', itemName: 'kitchen pot', category: 'cooking', quantity: 10 },
+// ]
+
+console.log(ItemManager.itemsInCategory('sports'));
+// logs:
+// [
+//   { skuCode: 'BASSP', itemName: 'basket ball', category: 'sports', quantity: 0 },
+//   { skuCode: 'FOOSP', itemName: 'football', category: 'sports', quantity: 3 },
+// ]
